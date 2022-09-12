aae47ddc904b9833026930fff55abd25b3eeae58
Unpei2 <r.ho5@share.epsb.ca>
2022-09-12 13:57:31 -0600

    done

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 // Triangle Perimeter Assignment Start Code
+
+
+
+
+
+document.getElementById("button").addEventListener("click",click)
+function click(){
+
+
+let Perimeter = document.getElementById("perimeter")
+
 let xA = +document.getElementById("xA").value
 let yA = +document.getElementById("yA").value
 let xB = +document.getElementById("xB").value
@@ -6,18 +17,17 @@ let yB = +document.getElementById("yB").value
 let xC = +document.getElementById("xC").value
 let yC = +document.getElementById("yC").value
 
-let AB = document.getElementById("abdist")
-let AC = document.getElementById("acdist")
-let BC = document.getElementById("bcdist")
-let Perimeter = document.getElementById("perimeter")
+let AB = dist(xA,yA,xB,yB)
+let AC = dist(xA,yA,xC,yC)
+let BC = dist(xB,yB,xC,yC)
+Perimeter.innerHTML = AB + AC + BC 
 
-document.getElementById("button").addEventListener("click",click)
-function click(){
-AB.innerHTML = dist(xA,yA,xB,yB)
-AC.innerHTML = "hello!"
-BC.innerHTML = "hellosdasd"
-Perimeter.innerHTML = "ab + ac + bc"
+document.getElementById("abdist").innerHTML = AB
+document.getElementById("acdist").innerHTML = AC
+document.getElementById("bcdist").innerHTML = BC
+
 }
+
 function dist(x1, y1, x2, y2){
     return Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
 }
